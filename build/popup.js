241a77d71e45efb755d09b45f1b6ba78cf989bdd
kromate <akpananthony33@gmail.com>
2021-04-11 22:58:23 +0100

    update

--- build/popup.js
+++ build/popup.js
@@ -1,14 +1,9 @@
 document.addEventListener('DOMContentLoaded', function() {
-    let url = window.location.pathname
-    let body = document.getElementById('counter')
-    
-    console.log(url);
-    console.log(body);
-
-    document.getElementById('incrementBtn').addEventListener('click',()=>{
+    document.body.addEventListener('click', ()=>{
         console.log('object');
         console.log(window.location.pathname);
     })
+
 
 })
 
