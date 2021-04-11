7558e3e489e86596dc0d3d3eabebee902332bcea
kromate <akpananthony33@gmail.com>
2021-04-11 22:37:27 +0100

    update

--- build/popup.js
+++ build/popup.js
@@ -1,7 +1,9 @@
+document.addEventListener('DOMContentLoaded', function() {
+    let url = window.location.pathname
+    let body = document.querySelector('#counter')
+    
+    console.log(url);
+    console.log(body);
 
+})
 
-let url = window.location.pathname
-// let body = document.querySelector('#counter')
-
-console.log(url);
-// console.log(body);
